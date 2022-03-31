26d25e8a205995661043368de7e4f064c87c5f1b
Pedro Plasencia <pjpv9011@gmail.com>
2022-03-31 23:47:04 +0200

    feat: watch doc

--- src/views/Watch/WatchView.vue
+++ src/views/Watch/WatchView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { ref, watch } from "vue";
 
+// Con watch podemos hacer seguimiento de objetos, refs, arrays, getters, computed refs, etc. de forma parecida al computed ref, pero con la posibilidad de mutar lo que necesitemos dentro del callback
+
 const order = ref("");
 const trooperAnswer = ref("");
 
+//"Observamos" el ref "order" para cambiar el valor de trooperAnswer dependiendo del número de orden identificado en el string
 watch(order, (newOrder) => {
   if (order.value.indexOf("66") > -1) {
     trooperAnswer.value = "Yes, my lord";
